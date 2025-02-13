7120f4334ff5caa944d5a40c725590ffd4655dd0
ankushKun <ankush4singh@gmail.com>
2025-02-13 09:34:46 +0530

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from '../contexts/AuthContext';
+import { Analytics } from "@vercel/analytics/react"
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Analytics />
     </AuthProvider>
   );
 }
